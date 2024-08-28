2f56cfe12edc96571fc8681462d652e875bb5657
MelikeeKorkmaz <melikekorkmaz099@gmail.com>
2024-08-28 14:27:17 +0300

    Update Gizli.java

--- Gizli.java
+++ Gizli.java
@@ -1,24 +1,26 @@
-import java.util.Arrays;
 import java.util.Random;
 
 public class MineSweeper {
     int row;
     int col;
     double mine;
+    boolean isGameEnd = true;
 
     String[][] matrix;
     String[][] game;
+    int[] list;
 
     MineSweeper(int row, int col) {
-        this.row = row;
-        this.col = col;
+        this.row = row + 2;
+        this.col = col + 2;
         this.mine = 0;
-        this.matrix = new String[row][col];
-        this.game = new String[row][col];
+        this.matrix = new String[this.row][this.col];
+        this.game = new String[this.row - 2][this.col - 2];
+        this.list = new int[row * col];
     }
 
     void calcMine() {
-        this.mine = Math.round((this.row * this.col) / 4.0);
+        this.mine = Math.round(((this.row - 2) * (this.col - 2)) / 4.0);
     }
 
     void placeMine() {
@@ -26,9 +28,9 @@ public class MineSweeper {
         Random rand = new Random();
         int i = 0;
         while (i < (this.mine)) {
-            int x = rand.nextInt(this.row);
-            int y = rand.nextInt(this.col);
-            if (!(matrix[x][y]).equals("*")) {
+            int x = rand.nextInt(this.row - 1);
+            int y = rand.nextInt(this.col - 1);
+            if (!(matrix[x][y]).equals("*") && (x != 0) && (y != 0)) {
                 this.matrix[x][y] = "*";
                 i++;
             }
@@ -44,73 +46,48 @@ public class MineSweeper {
         placeMine();
     }
 
-    boolean isBig(int a) {
-        if (a > (this.row-1)){
-            return false;
+    void frame() {
+        for (int i = 0; i < this.row; i++) {
+            for (int j = 0; j < this.col; j++) {
+                if (i == 0 || (i == this.row - 1)) {
+                    matrix[i][j] = "_";
+                } else if (j == 0 || (j == this.col - 1)) {
+                    matrix[i][j] = "|";
+                }
+            }
         }
-        if (a > (this.col-1)){
-            return false;
-        }
-        return true;
     }
 
-    boolean isSmall(int a) {
-        if (a < 0) {
-            return false;
+    void code(String[][] arr, int i, int j) {
+        if (arr[i][j] != "*") {
+            int x;
+            String str = arr[i][j];
+            int number = Integer.parseInt(str);
+            number++;
+            arr[i][j] = Integer.toString(number);
         }
-        return true;
     }
+
 
     void clue() {
         baseMatrix();
-
         for (int i = 0; i < this.row; i++) {
             for (int j = 0; j < this.col; j++) {
-
-                if(i != 0){
-                    this.matrix[i-1][j] += 1;
-                    if(j != 0){
-                        this.matrix[i-1][j-1] += 1;
-                    }
-                    if(j != col-1){
-                        this.matrix[i-1][j+1] += 1;
-                    }
+                if (matrix[i][j] == "*") {
+                    code(this.matrix, i - 1, j);
+                    code(this.matrix, i - 1, j - 1);
+                    code(this.matrix, i - 1, j + 1);
+                    code(this.matrix, i + 1, j - 1);
+                    code(this.matrix, i + 1, j);
+                    code(this.matrix, i + 1, j + 1);
+                    code(this.matrix, i, j - 1);
+                    code(this.matrix, i, j + 1);
                 }
-                if(i != row-1){
-                    this.matrix[i+1][j] += 1;
-                    if(j != 0){
-                        this.matrix[i+1][j-1] += 1;
-                    }
-                    if(j != col-1){
-                        this.matrix[i+1][j+1] += 1;
-                    }
-                }
-                if(j != 0){
-                    this.matrix[i][j-1] += 1;
-                }
-                if(j != col-1){
-                    this.matrix[i][j+1] += 1;
-                }
-
             }
         }
-
-
-
-
+        frame();
     }
 
-
-/*
-    void game(){
-        for(int i = 0; i < this.row; i++){
-            for(int j = 0; j < this.col; j++){
-                this.matrix[i][j] = "-";
-            }
-        }
-    }
-
- */
 
     void printMatrix() {
         clue();
@@ -122,8 +99,70 @@ public class MineSweeper {
         }
     }
 
+    void game() {
+        for (int i = 0; i < this.row - 2; i++) {
+            for (int j = 0; j < this.col - 2; j++) {
+                game[i][j] = "-";
+            }
+        }
+    }
+
+    void play(int x, int y) {
+        clue();
+        isNumSame(x, y);
+        if ((x < this.row - 2) && (y < this.col - 2) && (0 <= x) && (0 <= y) && (isNumSame(x, y))) {
+            if (this.matrix[x + 1][y + 1] == "*") {
+                System.out.println("Mayın!!! Oyunu kaybettiniz.");
+                printMatrix();
+                isGameEnd = false;
+            } else {
+                this.game[x][y] = this.matrix[x + 1][y + 1];
+                for (int i = 0; i < this.row - 2; i++) {
+                    for (int j = 0; j < this.col - 2; j++) {
+                        if (this.game[i][j] != "-") {
+                            System.out.println("***Tebrikler oyunu kazandınız***");
+                            isGameEnd = false;
+                        }
+                    }
+                }
+
+            }
+        } else {
+            if (!isNumSame(x, y)) {
+                System.out.println("Bu koordinat daha önce seçildi, başka bir koordinat girin!");
+            } else {
+                System.out.println("Yanlış değer girdiniz!");
+                System.out.println("Satır ve sütun sayınız 0 ve 0'dan büyük olmalı. Satır sayınız " + (this.row - 2) + " değerinden, sütun sayınız " + (this.col - 2) + " değerinden küçük olmalı.");
+            }
+        }
+    }
+
+    boolean isNumSame(int x, int y) {
+        //Liste önceden girilmediği için değerleri 0 gösteriyor olabilir. 0 a 0 noktası hata verebilir dene.
+        int number = ((x * 10) + y);
+        int count = 0;
+        for (int i : list) {
+            if (list[i] == number) {
+                return false;
+            } else {
+                list[count++] = number;
+                return true;
+            }
+        }
+        return true;
+    }
+
+    static boolean isMine(int x, int y) {
+        return true;
+    }
+
+    void printGame() {
+        game();
+        for (String[] i : this.game) {
+            for (String j : i) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
 }
-
-
-
-
